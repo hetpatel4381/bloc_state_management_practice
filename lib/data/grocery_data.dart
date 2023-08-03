49bf4d41430b5f4b9cffccba24ec11d234a7afb7
hetpatel4381 <hetpatel2808hp@gmail.com>
2023-08-04 00:19:10 +0530

    created the blocs of all the files

---
+++ lib/data/grocery_data.dart
@@ -0,0 +1,52 @@
+class GroceryData {
+  List<Map<String, dynamic>> sampleProductsData = [
+    {
+      "id": 1,
+      "name": "Fresh Apples",
+      "description":
+          "Crisp and delicious red apples, perfect for snacking or baking.",
+      "price": 2.99,
+      "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ868djAR5mIlAFnnjF---4paarCg48aQTr-g&usqp=CAU",
+    },
+    {
+      "id": 2,
+      "name": "Whole Milk",
+      "description":
+          "Creamy and nutritious whole milk, great for drinking or cooking.",
+      "price": 1.49,
+      "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkdnOTiZ0EWI2NFB2xF8Wfm1ELDx3gW53G_w&usqp=CAU",
+    },
+    {
+      "id": 3,
+      "name": "Loaf of Bread",
+      "description":
+          "Soft and freshly baked bread, ideal for sandwiches or toast.",
+      "price": 2.29,
+      "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQig7KLB9LNAFnlrzrzQQfbt-c8Ecj6hkCYpA&usqp=CAU",
+    },
+    {
+      "id": 4,
+      "name": "Chicken Breasts",
+      "description":
+          "Boneless, skinless chicken breasts, versatile for various recipes.",
+      "price": 6.99,
+      "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUZiRuu4WXnfUT5ePoBH3fjbZgmsn761iYuQ&usqp=CAU",
+    },
+    {
+      "id": 5,
+      "name": "Pasta",
+      "description":
+          "Classic pasta for hearty meals, pairs well with a variety of sauces.",
+      "price": 1.79,
+      "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRc8SJ8AA64oX1xmEwVMUhDbf_NIbwnza6c0A&usqp=CAU",
+    },
+    {
+      "id": 6,
+      "name": "Fresh Spinach",
+      "description":
+          "Nutrient-rich spinach leaves, perfect for salads, smoothies, or cooking.",
+      "price": 1.99,
+      "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlwbgVXHah8AD9Age2uRP1FCYv827E-0a-8g&usqp=CAU",
+    },
+  ];
+}
